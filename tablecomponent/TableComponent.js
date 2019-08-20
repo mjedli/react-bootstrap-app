4fb2424f8ed0bf4e466967328a6945aea4c31c50
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-20 15:58:58 +0200

    1 file modified

--- tablecomponent/TableComponent.js
+++ tablecomponent/TableComponent.js
@@ -16,20 +16,20 @@ class TableComponent extends Component {
   render() {
     return (
       
-  <div align="center" class ="tab-app col col-sm col-md col-lg col-xl tab-app">
-    <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a>test<i class="material-icons">autorenew</i></a>
-      </li>
-    </ul>
-    <div align="right">
-      <div class="btn-group" role="group" aria-label="Basic example">
-        <button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button>
-        <button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button>
-        <button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button>
+    <div align="center" class ="tab-app col col-sm col-md col-lg col-xl tab-app">
+        <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a>test<i class="material-icons">autorenew</i></a>
+        </li>
+      </ul>
+      <div align="right">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button>
+          <button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button>
+          <button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button>
+        </div>
       </div>
     </div>
-  </div>
 
     );
   }
