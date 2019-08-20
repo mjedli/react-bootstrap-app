8bde624492bec8ca1f27aab0f3dd72f008562964
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-20 15:54:15 +0200

    1 file modified

--- tablecomponent/TableComponent.js
+++ tablecomponent/TableComponent.js
@@ -17,7 +17,18 @@ class TableComponent extends Component {
     return (
       
   <div align="center" class ="tab-app col col-sm col-md col-lg col-xl tab-app">
-test
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a>test<i class="material-icons">autorenew</i></a>
+      </li>
+    </ul>
+    <div align="right">
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button>
+        <button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button>
+        <button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button>
+      </div>
+    </div>
   </div>
 
     );
