020673ec79d46c6d730dac9fe02d253b86d953c0
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-21 14:53:49 +0200

    1 file modified

--- table/Table.js
+++ table/Table.js
@@ -14,10 +14,10 @@ class Table extends Component {
   copyright = "Copyright";
 
   list = [
-    {id : 1, title : "title 3", adresse : "address 1", type : true},
-    {id : 2, title : "title 2", adresse : "address 4", type : false},
-    {id : 3, title : "title 1", adresse : "address 3", type : false},
-    {id : 4, title : "title 4", adresse : "address 2", type : false}
+    {id : 1, title : "title 3", adresse : "address 1", type : "true"},
+    {id : 2, title : "title 2", adresse : "address 4", type : "false"},
+    {id : 3, title : "title 1", adresse : "address 3", type : "false"},
+    {id : 4, title : "title 4", adresse : "address 2", type : "false"}
   ];
 
   render() {
