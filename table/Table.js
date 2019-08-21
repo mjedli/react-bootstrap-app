d7477148c611bc55df938730e4f449380d116b2f
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-21 14:04:33 +0200

    2 files added

---
+++ table/Table.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './table.css';
+
+class Table extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateCopyright: new Date().getFullYear(),
+    };
+  }
+
+  copyright = "Copyright";
+
+  render() {
+    return (
+      
+      <footer class="footer-app">
+        <div align="center">{this.copyright}&nbsp;{this.state.dateCopyright}&nbsp;&copy;</div>
+      </footer>
+
+    );
+  }
+}
+
+export default Table;
