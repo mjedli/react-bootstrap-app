93f5ec9463cfbf0ca4150b998462fcdc618ae8be
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-21 15:47:57 +0200

    1 file modified

---
+++ add/Add.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './footer.css';
+
+class Add extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateCopyright: new Date().getFullYear(),
+    };
+  }
+
+  render() {
+    return (
+      
+      <footer class="footer-app">
+        <div align="center">{this.copyright}&nbsp;{this.state.dateCopyright}&nbsp;&copy;</div>
+      </footer>
+
+    );
+  }
+}
+
+export default Add;
