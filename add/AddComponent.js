f1edf42df3ee4436268ce1599dda0825349bafb7
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-22 16:48:05 +0200

    1 file modified, 1 added, 1 deleted

--- add/AddComponent.js
+++ add/AddComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import './addComponent.css';
+import './add-component.css';
 
 class AddComponent extends Component {
 
