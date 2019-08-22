8dbdee83f3f20565e8763ead96f57364f5d925cb
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-22 16:54:10 +0200

    1 file modified

--- add/AddComponent.js
+++ add/AddComponent.js
@@ -18,16 +18,16 @@ class AddComponent extends Component {
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Title</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" value="{{currentComponent.title}}" name="currentComponent.title" >
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" value="{{currentComponent.title}}" name="currentComponent.title" />
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Address</label>
     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter address"
    value="{{currentComponent.adresse}}"
-    name="currentComponent.adresse">
+    name="currentComponent.adresse"/>
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" value="{{currentComponent.type}}" name="currentComponent.type">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" value="{{currentComponent.type}}" name="currentComponent.type"/>
     <label class="form-check-label" for="exampleCheck1">Type</label>
   </div>
   <div align="right">
