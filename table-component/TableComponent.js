af40c500c79985fc444720d1e79dc5dca45869ce
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-22 17:12:56 +0200

    1 file modified, 1 added

--- table-component/TableComponent.js
+++ table-component/TableComponent.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Table from '../table/Table'
 import AddComponent from '../add/AddComponent'
+import ModifyComponent from '../modify/ModifyComponent'
 import './table-component.css';
 
 class TableComponent extends Component {
@@ -32,7 +33,7 @@ class TableComponent extends Component {
         <div align="right">
           <div class="btn-group" role="group" aria-label="Basic example">
             <Link to="/add"><button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button></Link>
-            <button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button>
+            <Link to="/modify"><button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button></Link>
             <button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button>
           </div>
         </div>
@@ -40,6 +41,7 @@ class TableComponent extends Component {
 
         <Route exact path="/" component={Table}/>
         <Route path="/add" component={AddComponent}/>
+        <Route path="/modify" component={ModifyComponent}/>
 
     </Router>
     </div>
