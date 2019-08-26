505f3ba05ca562b370e5d946044e76c5ecb63b69
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-26 14:15:05 +0200

    2 files added

---
+++ remove/RemoveComponent.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './remove-component.css';
+
+class RemoveComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateCopyright: new Date().getFullYear(),
+    };
+  }
+
+  render() {
+    return (
+      
+      <div class="col col-sm col-md col-lg col-xl tab-app-add" align="left">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Title</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" value="" name="currentComponent.title" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Address</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter address"
+        value=""
+          name="currentComponent.adresse"/>
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" value="{{currentComponent.type}}" name="currentComponent.type"/>
+          <label class="form-check-label" for="exampleCheck1">Type</label>
+        </div>
+        <div align="right">
+          <button type="button" routerLink="/" class="btn btn-info">Cancel</button>
+          &nbsp;
+          <button type="submit" class="btn btn-primary" >Add</button>
+        </div>
+      </form>
+      </div>
+
+    );
+  }
+}
+
+export default RemoveComponent;
