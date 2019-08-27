b5359d90332eb68b696389f0955c42dbe5d89d12
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-27 17:40:20 +0200

    1 file modified

--- modify/ModifyComponent.js
+++ modify/ModifyComponent.js
@@ -21,10 +21,20 @@ class ModifyComponent extends Component {
     this.state.appService.modifiyComponent(this.state.currentComponent);
   }
 
-_handleChangeEvent(event) {
-  console.log(event.target.value);
-        this.state.currentComponent.title = event.target.value;
-      }
+  titleChangeEvent(event) {
+    console.log(event.target.value);
+    this.state.currentComponent.title = event.target.value;
+  }
+
+  addressChangeEvent(event) {
+    console.log(event.target.value);
+    this.state.currentComponent.adresse = event.target.value;
+  }
+
+  typeChangeEvent(event) {
+    console.log(event.target.checked);
+    this.state.currentComponent.type = event.target.checked.value;
+  }
 
   render() {
     return (
@@ -34,16 +44,17 @@ _handleChangeEvent(event) {
         <div class="form-group">
           <label for="exampleInputEmail1">Title</label>
           <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" defaultValue ={this.state.currentComponent.title} name="currentComponent.title"
-          onChange={(e)=>{this._handleChangeEvent(e)}} />
+          onChange={(e)=>{this.titleChangeEvent(e)}} />
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Address</label>
-          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter address"
-        defaultValue ={this.state.currentComponent.adresse}
+          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter address" defaultValue ={this.state.currentComponent.adresse} 
+          onChange={(e)=>{this.addressChangeEvent(e)}}
           name="currentComponent.adresse"/>
         </div>
         <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" defaultValue ={this.state.currentComponent.type} name="currentComponent.type"/>
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" defaultValue ={this.state.currentComponent.type} name="currentComponent.type"
+          onChange={(e)=>{this.typeChangeEvent(e)}}/>
           <label class="form-check-label" for="exampleCheck1">Type</label>
         </div>
         <div align="right">
