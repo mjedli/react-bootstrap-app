4086d2afd93eb4abcb49205cfcb259b1e210c858
mjedli <jedli.mejdi.recup@gmail.com>
2019-08-29 15:34:55 +0200

    1 file modified

--- table-component/TableComponent.js
+++ table-component/TableComponent.js
@@ -24,30 +24,33 @@ class TableComponent extends Component {
     
     
     <div className="TableComponent" class ="tab-app">
-    <Router>
-      <div align="center" class ="col col-sm col-md col-lg col-xl">
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
-            <Link class="nav-link active" to="/">{this.title}
-            <i class="material-icons">autorenew</i></Link>
-          </li>
-        </ul>
-        <div align="right">
-          <div class="btn-group" role="group" aria-label="Basic example">
-            <Link to="/add"><button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button></Link>
-            <Link to="/modify"><button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button></Link>
-            <Link to="/remove"><button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button></Link>
+
+      <Router>
+
+        <div align="center" class ="col col-sm col-md col-lg col-xl">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <Link class="nav-link active" to="/">{this.title}
+              <i class="material-icons">autorenew</i></Link>
+            </li>
+          </ul>
+          <div align="right">
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <Link to="/add"><button  type="button" class="btn btn-secondary">Add<i class="material-icons">add</i></button></Link>
+              <Link to="/modify"><button  type="button" class="btn btn-secondary">Modify<i class="material-icons">autorenew</i></button></Link>
+              <Link to="/remove"><button  type="button" class="btn btn-secondary">Remove<i class="material-icons"><i class="material-icons">remove_circle_outline</i></i></button></Link>
+            </div>
           </div>
         </div>
-      </div>
 
-        <Route exact path="/" component={Table}/>
-        <Route path="/add" component={AddComponent}/>
-        <Route path="/modify" component={ModifyComponent}/>
-        <Route path="/remove" component={RemoveComponent}/>
-        <Route path="/alert" component={AlertComponent}/>
+          <Route exact path="/" component={Table}/>
+          <Route path="/add" component={AddComponent}/>
+          <Route path="/modify" component={ModifyComponent}/>
+          <Route path="/remove" component={RemoveComponent}/>
+          <Route path="/alert" component={AlertComponent}/>
 
-    </Router>
+      </Router>
+
     </div>
 
     );
