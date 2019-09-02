5f6f4c334b7e154dffa1a060b66c5d506b166211
mjedli <jedli.mejdi.recup@gmail.com>
2019-09-02 15:04:56 +0200

    1 file modified

--- toolbar/Toolbar.js
+++ toolbar/Toolbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AppService from '../AppService';
 import './toolbar.css';
 
@@ -8,7 +10,7 @@ class Toolbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      appService : AppService,
+      appService : AppService
     };
   }
 
@@ -18,21 +20,21 @@ class Toolbar extends Component {
 
   search() {
     this.state.appService.setSearchValue(this.searchValue);
-    this.props.history.push('/');
   }
 
   searchValueChangeEvent(event) {
     this.searchValue = event.target.value;
   }
 
+
   render() {
     return (
       
-      <nav className="Toolbar" class="navbar  toolbar-app">
+      <nav className="Toolbar" class="navbar toolbar-app">
         <a class="navbar-brand">{this.props.title}</a>
         <form class="form-inline">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" defaultValue={this.searchValue} onChange={(e) => this.searchValueChangeEvent(e)} name="searchValue" />
-          <button  onClick={() => this.search()} class="btn btn-outline-success my-2 my-sm-0" type="boutton">{this.searchLabel}</button>
+          <button  onClick={() => this.search()} class="btn btn-outline-success my-2 my-sm-0" type="button">{this.searchLabel}</button>
         </form>
       </nav>
 
